075e78aa40ee14cec77cf8da743c59a94b515afc
kingsman44 <shivan.0972@gmail.com>
2024-02-27 13:03:33 +0530

    add media query

--- src/components/NameNavbar.jsx
+++ src/components/NameNavbar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/fundkaro.svg"
 
 function NameNavbar() {
   return (
-    <header className="bg-gradient-to-r from-darkPrimary to-lightPrimary flex h-16 items-center"> 
+    <header className="fixed top-0 w-full bg-gradient-to-r from-darkPrimary to-lightPrimary flex h-16 items-center"> 
       <img src={logo} className="w-32 h-9 ml-8 " /> 
     </header>
   )
