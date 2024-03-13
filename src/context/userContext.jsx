928068b074d3d66b6d37e1b14e734b81a62073a6
kingsman44 <shivan.0972@gmail.com>
2024-03-14 02:47:37 +0530

    fix information on reload [1/2]

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -3,20 +3,28 @@ import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({})
 
-export function UserContextProvider({children}){
-  const [currentuser,setCurrentUser] = useState({email:"", phone:"", referrer:"", refferal_code: ""});
-  const [currentuserdetail, setCurrentUserDetail] = useState({name: "",panNo: "",aadharNo: "",internalRating: 0,photo: 'https://res.cloudinary.com/drfokf5ix/image/upload/v1709286872/profile_placeholder.png', dob: null,isPanVerified: false,cibilScore: null});
-  useEffect(()=>{
-    if(!currentuser){
-      axios.get('/profile').then(({data})=>{
-        setUser(data)
-        // console.log(data);
-      })
+export function UserContextProvider({ children }) {
+  const [currentuser, setCurrentUser] = useState({ email: "", phone: "", referrer: "", refferal_code: "" });
+  const [currentuserdetail, setCurrentUserDetail] = useState({ name: "", panNo: "", aadharNo: "", internalRating: 0, photo: 'https://res.cloudinary.com/drfokf5ix/image/upload/v1709286872/profile_placeholder.png', dob: null, isPanVerified: false, cibilScore: null });
+  useEffect(() => {
+    if (currentuser.email == "") {
+      //console.log('Hi');
+      try {
+        axios.get('/profile').then(({ data }) => {
+          if (data && data['user'] && data['userdetail']) {
+            setCurrentUser(data['user'])
+            setCurrentUserDetail(data['userdetail'])
+            console.log(data);
+          }
+        })
+      } catch (e) {
+        console.log(e)
+      }
     }
-  },[]);
+  }, []);
   return (
-    <UserContext.Provider value={{currentuser, setCurrentUser, currentuserdetail, setCurrentUserDetail }}>
-    {children}
+    <UserContext.Provider value={{ currentuser, setCurrentUser, currentuserdetail, setCurrentUserDetail }}>
+      {children}
     </UserContext.Provider>
   )
 }
